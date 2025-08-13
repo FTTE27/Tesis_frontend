690710a60cbc16fa42797ee9d8da70a2fe5c8ec3
Andrés Ruge <119058759+Rujnp794@users.noreply.github.com>
2025-08-13 14:22:38 -0500

    Pantalla records

---
+++ frontend/src/app/admin/records/records.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderAdmin } from '../header-admin/header-admin';
+
+@Component({
+  selector: 'app-records',
+  imports: [HeaderAdmin],
+  templateUrl: './records.html',
+  styleUrl: './records.css'
+})
+export class Records {
+
+}
