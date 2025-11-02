edcd1c21cc85ba30cc0a1e3b5ed8f389bb4b6961
Andres Ruge <anferu2004@gmail.com>
2025-11-02 15:05:03 -0500

    Logo y Color agregados.

--- frontend/src/app/admin/management/management.ts
+++ frontend/src/app/admin/management/management.ts
@@ -5,11 +5,12 @@ import { Router } from '@angular/router';
 import { HeaderAdmin } from "../header-admin/header-admin";
 import { UserService, User } from '../../services/user_services';
 import { LoginService } from '../../services/login_services';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-management',
   standalone: true,
-  imports: [CommonModule, FormsModule, HeaderAdmin],
+  imports: [CommonModule, FormsModule, HeaderAdmin,],
   templateUrl: './management.html',
   styleUrls: ['./management.css']
 })
@@ -20,17 +21,21 @@ export class Management implements OnInit {
   password: string = '';
   users: User[] = [];
   selectedUserId: number | null = null;
+  models : string[] = [];
+  selectedModel: string | null = null;
 
   //Inyectamos los servicios necesarios:
   constructor(
-    private router: Router, 
-    private userService: UserService,  
-    private authService: LoginService
+    private router: Router,
+    private userService: UserService,
+    private authService: LoginService,
+    private http: HttpClient
   ) {}
-    
+
   //Ejecuta al iniciar el componente
   ngOnInit(): void {
     this.loadUsers();
+    this.loadModels();
   }
 
   // Cargar usuarios
@@ -89,7 +94,7 @@ export class Management implements OnInit {
 
   // Editar → cargar datos al formulario
   editUser() {
-    // busca el usuario seleccionado y carga sus datos 
+    // busca el usuario seleccionado y carga sus datos
     if (!this.selectedUserId) return;
     const user = this.users.find(u => u.id === this.selectedUserId);
     if (user) {
@@ -138,5 +143,39 @@ export class Management implements OnInit {
       }
     });
   }
+ // Cargar lista de modelos
+ loadModels(): void {
+  this.http.get<{ models: string[] }>('http://localhost:8000/models/get_models')
+    .subscribe({
+      next: (response) => {
+        this.models = response.models;
+      },
+      error: (err) => {
+        console.error('Error al cargar modelos', err);
+        alert('Error al cargar modelos disponibles.');
+      }
+    });
+}
 
+// Cambiar modelo seleccionado
+changeModel(): void {
+  if (!this.selectedModel) {
+    alert('Por favor selecciona un modelo primero.');
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append('model_name', this.selectedModel);
+
+  this.http.post('http://localhost:8000/models/change_model', formData)
+    .subscribe({
+      next: (res: any) => {
+        alert(res.message || 'Modelo cambiado correctamente.');
+      },
+      error: (err) => {
+        console.error('Error al cambiar modelo', err);
+        alert('No se pudo cambiar el modelo.');
+      }
+    });
+}
 }
